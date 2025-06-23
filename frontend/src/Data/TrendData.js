2290c99986bcd9996a4a2e4930b12c2125354b23
theluchadora <160585859+theluchadora@users.noreply.github.com>
2025-06-23 12:35:28 +0300

    finished the RightSide of Home Page

---
+++ frontend/src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData = [
+    {
+        name: "Minions",
+        share: 97,
+    },
+     {
+        name: "Avengers",
+        share: 80,
+    },
+     {
+        name: "loveiland",
+        share: 75.5,
+    },
+     {
+        name: "Reactjs",
+        share: 72,
+    },
+     {
+        name: "Elon Musk",
+        share: 71.9,
+    },
+     {
+        name: "30 day Leetcode challange",
+        share: 20,
+    }
+]
