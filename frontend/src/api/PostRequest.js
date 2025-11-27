341228bbbb6120c25fb3d2ccb180dd4327d05412
theluchadora <160585859+theluchadora@users.noreply.github.com>
2025-11-27 14:01:13 +0300

    change connection to backend for render

--- frontend/src/api/PostRequest.js
+++ frontend/src/api/PostRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL });
 
 export const getTimelinePosts = async (id) => API.get(`/post/${id}/timeline`);
 export const likePost = async (id, userId) => API.put(`/post/${id}/like`, { userId: userId });
--- frontend/src/api/UploadRequest.js
+++ frontend/src/api/UploadRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL });
 
 export const uploadImage = (data) => API.post("/upload/", data);
 
